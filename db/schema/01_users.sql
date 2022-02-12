563990b48e8ea1ac6bd8f403102bac5ec2bce418
0lgaP <perronolga@gmail.com>
2022-02-12 00:13:14 +0000

    01_users.sql schema & 01_users.sql seeds

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,5 +3,10 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone_number INTEGER,
+  favourite_items INTEGER REFERENCES items(id) on DELETE CASCADE,
+  messages_id INTEGER REFERENCES messages(id) on DELETE CASCADE
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users ('name', 'email', 'password', 'phone_number')
+VALUES ;
+('Ash Ketchum', 'ash@gmail.com', '1234', 6477879090)
+('Misty', 'misty@gmail.com', '1234', 6477879030)
+('Brock', 'brock@gmail.com', '1234', 6477878970)
+('Professor Samuel Oak', 'oak@gmail.com', '1234', 6477878910)
+('Jessie', 'jessie@gmail.com', '1234', 6477878850)
+('James', 'james@gmail.com', '1234', 6477878790)
