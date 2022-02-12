4a9bd092f9b9bb907f6d6993fcd97dec9a1c32b0
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-12 16:51:31 -0500

    Add items_new ejs file

---
+++ routes/items_new.js
@@ -0,0 +1,16 @@
+/*
+ * All routes for Widgets are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    res.render("items_new");
+  });
+  return router;
+};
--- server.js
+++ server.js
@@ -37,11 +37,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const itemsNewRoutes = require("./routes/items_new");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/items/new", itemsNewRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,6 +54,12 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// New pokemon page
+app.get("/items_new", (req, res) => {
+  res.render("items_new");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
