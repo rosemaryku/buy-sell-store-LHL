b5efa690d97b5416922b352b3a66f1bdbe9d503c
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-15 14:24:37 -0500

    add login cookie

--- routes/login.js
+++ routes/login.js
@@ -6,11 +6,33 @@
  */
 
 const express = require('express');
+const users = require('./users');
 const router  = express.Router();
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
     res.render("login")
   });
+
+
+  // Login submission
+  router.post("", (req, res) => {
+    const queryStr = `SELECT * FROM users WHERE email = $1;`
+    const values = [`${req.body.email}`]
+
+    db.query(queryStr, values)
+      .then(data => {
+        const userId = data.rows[0].id
+        req.session.user_id = userId;
+        res.redirect(`users/${userId}/items/new`)
+      })
+      .catch(err => {
+        res.status(500).send("Wrong email");
+      })
+  });
+
   return router;
+
+
 };
