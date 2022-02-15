588ea0c0ae79273ac1f041a2651a89c07958c43b
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-02-14 19:15:46 -0500

    users/messages populates with the pokemon the user actually owns

---
+++ db/seeds/04_messages.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO messages (user_id, item_id, user_message, sent_at)
+-- VALUES
+-- (4, 1, "Hello. Is Bulbasaur still available?", 2021-12-12 00:00:01),
+-- (3, 1, "Yes, $1000 firm", 2021-12-12 00:01:01),
+-- (4, 1, "I'm okay with that, what time should we meet?", 2021-12-12 00:02:01),
+-- (3, 1, "7pm, meet me by the dumpster", 2021-12-12 00:03:01);
