03e27738484a8dc9dbd88fda09aab95b0aad1744
0lgaP <perronolga@gmail.com>
2022-02-15 18:05:44 +0000

    register updates database with new user information

--- routes/register.js
+++ routes/register.js
@@ -12,5 +12,25 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("register")
   });
+
+  router.post("/", (req, res) => {
+    const queryStr = `
+      INSERT INTO users (name, email, password, phone_number)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;`
+
+    const values = [`${req.body.new_name}`, `${req.body.new_email}`, `${req.body.new_password}`, `${req.body.new_phone}`];
+
+
+    db.query(queryStr, values)
+      .then(data => {
+        console.log("DATA", data.rows)
+        res.render("login");
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
   return router;
 };
