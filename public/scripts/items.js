6061bfc8ebd3338514625a307dd7eb3812b5ef42
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-14 22:19:27 -0500

    edit CSS for id page

--- public/scripts/items.js
+++ public/scripts/items.js
@@ -3,15 +3,12 @@ $(document).ready(function () {
   console.log("Ready");
 
   // Change heart colour when favourited and submit favourite
-  // TODO Need to be restrict favouriting to 1 time and unfavourite
-  $(".favHeart").on("click", function() {
+  $(".favHeart").one("dblclick", function() {
     this.submit();
-    $(".fa-heart").toggleClass("favourited");
+    $(".fa-heart").addClass("favourited");
   });
 
-
-
-  });
+});
 
 
 
--- routes/items_id.js
+++ routes/items_id.js
@@ -1,24 +1,28 @@
 const express = require('express');
+const { redirect } = require('express/lib/response');
 const router  = express.Router();
 
 module.exports = (db) => {
 
  router.get("/:id", (req, res) => {
-  const queryStr = `SELECT *, items.id AS items_id FROM items JOIN users ON owner_id = users.id WHERE items.id = $1 ;`
+  const queryStr = `SELECT *, items.id AS items_id FROM items JOIN users ON owner_id = users.id WHERE items.id = $1;`
   const values = [req.params.id];
   db.query(queryStr, values)
   .then(data => {
     const specificItem = data.rows[0];
-    console.log(specificItem);
+    console.log("specificItem", specificItem);
     // console.log("Req Params ID:", req.params.id);
     const templateVars = {
       specificItem: specificItem,
     }
-    res.render("items_id", templateVars);
+    if (specificItem){
+      res.render("items_id", templateVars);
+    } else {
+      res.send("Error: Item does not exist");
+    }
     })
   .catch(err => {
-    res
-      .status(500)
+    res.status(500)
       .json({ error: err.message });
     });
   });
