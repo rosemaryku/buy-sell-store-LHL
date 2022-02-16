d3f65fb64f4bed4f7fc2caaeabea3b25fc1c9d06
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-16 09:32:07 -0500

    edit favourites logic to exclude duplicates

--- routes/users_id_favourites.js
+++ routes/users_id_favourites.js
@@ -3,26 +3,23 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/:id/favourites", (req, res) => {
-
-    const queryStr = `SELECT *
-      FROM favourites
-      JOIN items ON item_id = items.id
-      WHERE user_id = $1
-      ;`
-
+    const queryStr = `SELECT DISTINCT user_id, item_id, picture_url, title, price_per_item, quantity, posted_at
+    FROM favourites
+    JOIN items ON item_id = items.id
+    WHERE user_id = $1
+    ;`
     const values = [`${req.session.user_id}`];
-
     db.query(queryStr, values)
       .then(data => {
         const items = data.rows;
         // console.log("ID:", req.params.id);
-        // console.log(items);
+        // console.log("ITEMS:", items);
         const templateVars = {
           items: items,
           itemId: req.params.id,
           userId: req.session.user_id
         };
-        // console.log(templateVars);
+        // console.log("TEMPLATE VARS:", templateVars);
         res.render("users_favourites", templateVars);
       })
       .catch(err => {
@@ -34,8 +31,7 @@ module.exports = (db) => {
 
 
   router.post("/:id/favourites/:itemId", (req, res) => {
-    const queryStr = `
-      INSERT INTO favourites (user_id, item_id)
+    const queryStr = `INSERT INTO favourites (user_id, item_id)
       VALUES ($1, $2)
       RETURNING *;`
     const values = [`${req.session.user_id}`, `${req.params.itemId}`];
@@ -50,14 +46,11 @@ module.exports = (db) => {
 
 
    router.delete("/favourites/:itemId/delete", (req, res) => {
-    console.log("deleting");
-    const queryStr = `
-      DELETE FROM favourites WHERE user_id = $1 AND item_id = $2;`
+    const queryStr = `DELETE FROM favourites WHERE user_id = $1 AND item_id = $2;`
     const values = [`${req.session.user_id}`, `${req.params.itemId}`];
-    console.log("Values:", values);
+    console.log("VALUES:", values);
     db.query(queryStr, values)
       .then(data => {
-        console.log("Deleted");
         res.redirect('back');
       })
       .catch(err => {
